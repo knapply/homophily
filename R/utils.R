820f546c3028118d2409613b2b43f808a83826ef
Brendan <brendan.g.knapp@gmail.com>
2019-09-12 05:27:41 -0700

    document/expoert as_mixing_matrix

--- R/utils.R
+++ R/utils.R
@@ -38,19 +38,43 @@ as_attr_el.network <- function(g, node_attr_name) {
   as_attr_el.igraph(intergraph::asIgraph(g), node_attr_name)
 }
 
+
+#' Construct a graph object's mixing matrix.
+#' 
+#' Using a categorical vertex attribute, construct a matrix depicting network mixing
+#' patterns.
+#' 
+#' @param g 
+#' * `<igraph>`
+#'   + `<network>`, if `{intergraph}` is installed.
+#' @param node_attr_name 
+#' * `<chr>`
+#' * The name of a node/vertex attribute in `g`. 
+#' 
+#' @template author-bk
+#' 
+#' @examples 
+#' as_mixing_matrix(jemmah_islamiyah, node_attr_name = "role")
+#' 
+#' @export
 as_mixing_matrix <- function(g, node_attr_name) {
   UseMethod("as_mixing_matrix")
 }
 
+#' @rdname as_mixing_matrix
+#' @export
 as_mixing_matrix.igraph <- function(g, node_attr_name) {
   attr_el <- as_attr_el.igraph(g, node_attr_name)
   dim_names <- unique(sort(attr_el))
   
-  table(factor(attr_el[, 1], levels = dim_names), 
-        factor(attr_el[, 2], levels = dim_names))
+  out <- table(factor(attr_el[, 1], levels = dim_names), 
+               factor(attr_el[, 2], levels = dim_names))
+  class(out) <- "matrix"
+  out
 }
 
-
+#' @rdname as_mixing_matrix
+#' @export
 as_mixing_matrix.network <- function(g, node_attr_name) {
   if (!requireNamespace("intergraph", quietly = TRUE)) {
     stop("{intergraph} is required to perform this operation on objects of 
